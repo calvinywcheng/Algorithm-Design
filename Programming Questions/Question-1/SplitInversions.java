e3d7b98353b0c5aace35dad82464c14b8e05972b
Calvin Cheng <calvin.cheng.cc@outlook.com>
2014-05-10 01:19:41 -0700

    Added problem description

--- Programming Questions/Question-1/SplitInversions.java
+++ Programming Questions/Question-1/SplitInversions.java
@@ -1,10 +1,35 @@
+import java.io.*;
 import java.util.*;
 
+/**
+ * This program compute the number of inversions in the file given,
+ * where the <i>i</i><sup>th</sup> row of the file indicates the
+ * <i>i</i><sup>th</sup> entry of an array.
+ * 
+ * @author Calvin Cheng
+ */
 public class SplitInversions {
+	
+	private static int countInversions(int[] intArray) {
+		return 0;
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Scanner input = new Scanner(new File("IntegerArray.txt"));
+		int[] intArray = new int[100000];
+		
+		for (int i = 0; i < intArray.length; i++) {
+			intArray[i] = input.nextInt();
+		}
+		
+		int splitInversions = countInversions(intArray);
+		
+		System.out.println(splitInversions);
+		
+		input.close();
+		
+		return;
 	}
 
 }
