1bd900a41828d7c8e5b68e3272b36278d61e84f5
Calvin Cheng <calvin.cheng.cc@outlook.com>
2014-06-01 16:05:04 -0700

    Programming Question 3

---
+++ Programming Questions/Question-3/GraphMinCuts.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class GraphMinCuts {
+
+	public static void main(String[] args) {
+		
+		Map<Integer, LinkedList<Integer>> graph = new HashMap<Integer, LinkedList<Integer>>();
+
+	}
+
+}
