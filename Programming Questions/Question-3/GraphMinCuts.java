259ffeeda842d603122ef5d325b5efad93ec5fb9
Calvin Cheng <calvin.cheng.cc@outlook.com>
2014-06-02 11:48:39 -0700

    Parse input

--- Programming Questions/Question-3/GraphMinCuts.java
+++ Programming Questions/Question-3/GraphMinCuts.java
@@ -1,11 +1,28 @@
 import java.util.*;
+import java.io.*;
 
 public class GraphMinCuts {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
-		Map<Integer, LinkedList<Integer>> graph = new HashMap<Integer, LinkedList<Integer>>();
-
+		Map<Integer, List<Integer>> graph = new HashMap<Integer, List<Integer>>(200, 1.0f);
+		
+		Scanner input = new Scanner(new File("kargerMinCut.txt"));
+		
+		while (input.hasNextLine()) {
+			Scanner parseLine = new Scanner(input.nextLine());
+			int node = parseLine.nextInt();
+			List<Integer> adjacent = new LinkedList<Integer>();
+			while (parseLine.hasNext())
+				adjacent.add(parseLine.nextInt());
+			graph.put(node, adjacent);
+			parseLine.close();
+		}
+		
+		System.out.println(graph);
+		
+		input.close();
+		
 	}
 
 }
