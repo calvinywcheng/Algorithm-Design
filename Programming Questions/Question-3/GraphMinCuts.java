1f9ced5c6f67277919665ee8167a8bf558710109
Calvin Cheng <calvin.cheng.cc@outlook.com>
2014-06-04 14:07:28 -0700

    Finished Assignment 3

--- Programming Questions/Question-3/GraphMinCuts.java
+++ Programming Questions/Question-3/GraphMinCuts.java
@@ -3,7 +3,8 @@ import java.io.*;
 
 public class GraphMinCuts {
 	
-	private static final int N = 200;
+	private static final int N = 1000;
+	private static Random rand = new Random(System.currentTimeMillis());
 	
 	private static Map<Integer, List<Integer>> getGraph(File file) throws FileNotFoundException {
 
@@ -29,25 +30,46 @@ public class GraphMinCuts {
 	private static int getCut(Map<Integer, List<Integer>> graph) {
 		
 		if (graph.size() <= 2) {
-			return graph.get(0).size();
+			Iterator<Map.Entry<Integer, List<Integer>>> iter = graph.entrySet().iterator();
+			return iter.next().getValue().size();
 		}
 		else {
-			int u = (int)(Math.random() * graph.size());
-			int v = graph.get(u).get((int)(Math.random() * graph.get(u).size()));
+			int u = getRandomKey(graph);
+			int v = graph.get(u).get(rand.nextInt(graph.get(u).size()));
 			contract(graph, u, v);
 			return getCut(graph);
 		}
 	}
 	
+	private static int getRandomKey(Map<Integer, List<Integer>> graph) {
+		Iterator<Integer> iter = graph.keySet().iterator();
+		int rn = rand.nextInt(graph.size());
+		for (int i = 0; i < rn; i++) {
+			iter.next();
+		}
+		return iter.next();
+	}
+	
 	private static void contract(Map<Integer, List<Integer>> graph, int u, int v) {
-		graph.get(u).addAll(graph.get(graph.get(v)));
+		graph.get(u).addAll(graph.get(v));
 		graph.remove(v);
+		fixEdges(graph, u, v);
 		removeSelfLoops(graph, u);
+	}
+	
+	private static void fixEdges(Map<Integer, List<Integer>> graph, int u, int v) {
+		Iterator<Map.Entry<Integer, List<Integer>>> entry = graph.entrySet().iterator();
+		while (entry.hasNext()) {
+			List<Integer> li = entry.next().getValue();
+			while (li.contains(v)) {
+				li.set(li.indexOf(v), u);
+			}
+		}
 	}
 	
 	private static void removeSelfLoops(Map<Integer, List<Integer>> graph, int u) {
 		while (graph.get(u).contains(u)) {
-			graph.remove(graph.get(u).indexOf(u));
+			graph.get(u).remove(graph.get(u).indexOf(u));
 		}
 	}
 
@@ -58,12 +80,13 @@ public class GraphMinCuts {
 		
 		for (int repeat = 0; repeat < N; repeat++) {
 			int cut = getCut(getGraph(inputFile));
+			//System.out.println("Iteration " + repeat + ": " + cut);
 			if (cut < minCut)
 				minCut = cut;
 		}
 		
 		System.out.println("After " + N + " iterations of the random contraction algorithm,"
-				+ "the minimum cut for the graph was found to be " + minCut + ".");
+				+ " the minimum cut for the graph was found to be " + minCut + ".");
 	}
 
 }
